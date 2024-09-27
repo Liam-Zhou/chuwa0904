777e678a868deeb0348fa5f7baf7178aff24c857
Brent Liu <lcncufe@gmail.com>
2024-09-27 17:09:12 -0500

    Chenning Liu HW10

--- Coding/spring_demo/com/example/demo/model/ServiceA.java
+++
@@ -1,8 +0,0 @@
-
-package com.example.demo.model;
-
-public class ServiceA {
-    public void doService() {
-        System.out.println("Service A executed.");
-    }
-}
--- Coding/spring_demo/com/example/demo/model/ServiceB.java
+++
@@ -1,8 +0,0 @@
-
-package com.example.demo.model;
-
-public class ServiceB {
-    public void doService() {
-        System.out.println("Service B executed.");
-    }
-}
