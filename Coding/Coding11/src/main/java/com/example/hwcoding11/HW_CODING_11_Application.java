a73b88f601757be81d983fed542dbd0803b94988
PatrickLyh <118316330+PatrickLyh@users.noreply.github.com>
2024-10-02 16:10:19 -0700

    add HW11.md and Coding11

---
+++ Coding/Coding11/src/main/java/com/example/hwcoding11/HW_CODING_11_Application.java
@@ -0,0 +1,11 @@
+package com.example.hwcoding11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HW_CODING_11_Application {
+    public static void main(String[] args) {
+        SpringApplication.run(HW_CODING_11_Application.class, args);
+    }
+}
