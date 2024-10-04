4671ecf3fb51d1f69d7504c2488cd4eb5f1407a5
Mingxiao <hellomingxiao@gmail.com>
2024-10-04 19:49:45 -0400

    Mingxiao_Li/hw12

---
+++ Coding/hw12/https/src/main/java/com/hw11/https/HttpsApplication.java
@@ -0,0 +1,15 @@
+package com.hw11.https;
+
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpsApplication.class, args);
+	}
+
+}
+
