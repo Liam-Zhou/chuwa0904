140539f00178e27d75576108d6e89bb95667e894
lanxx116 <nicholas9849@gmail.com>
2024-10-04 04:01:26 -0700

    hw12

--- Coding/SpringSecurityDemo/src/main/java/org/example/springsecuritydemo/SpringSecurityDemoApplication.java
+++ Coding/SpringSecurityDemo/src/main/java/org/example/springsecuritydemo/SpringSecurityDemoApplication.java
@@ -2,7 +2,7 @@ package org.example.springsecuritydemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 @SpringBootApplication
 public class SpringSecurityDemoApplication {
 
---
+++ Coding/SpringSecurityDemo/src/main/java/org/example/springsecuritydemo/aop/LoggingAspect.java
@@ -0,0 +1,18 @@
+package org.example.springsecuritydemo.aop;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+
+import java.util.Arrays;
+
+public class LoggingAspect {
+
+}
