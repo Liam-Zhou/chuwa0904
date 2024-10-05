2e5297863efc31e9a858bfb5cd84168c829eed8f
lanxx116 <nicholas9849@gmail.com>
2024-10-04 18:29:41 -0700

    HW12

--- Coding/SpringSecurityDemo/src/main/java/org/example/springsecuritydemo/aop/LoggingAspect.java
+++ Coding/SpringSecurityDemo/src/main/java/org/example/springsecuritydemo/aop/LoggingAspect.java
@@ -1,11 +1,16 @@
 package org.example.springsecuritydemo.aop;
+import jakarta.servlet.http.HttpServletRequest;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
 
 import java.util.Arrays;
 @Aspect
@@ -15,27 +20,33 @@ public class LoggingAspect {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
     // Pointcut to match all public methods in the application and external code
-    @Pointcut("execution(public * org.example.springsecuritydemo.*.*(..))")
-    private void publicMethodsFromLoggingPackage() {}
+//    @Pointcut("execution(public * org.example.springsecuritydemo.*.*(..))")
+//    public void serviceLayer() {}
 
-    @Before(value = "publicMethodsFromLoggingPackage()")
+    @Before("execution(public  * org.example.springsecuritydemo.controller.HelloController.sayHello())")
     public void logBefore(JoinPoint joinPoint) {
         logger.info("Logging before method execution.");
     }
-    @Around(value = "publicMethodsFromLoggingPackage()")
+    @Around("execution(public  * org.example.springsecuritydemo.controller.HelloController.sayHello())")
     public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
-        logger.debug("Logging before method execution.");
+        long startTime = System.currentTimeMillis();
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         Object result = joinPoint.proceed();
-        logger.debug("Logging after method execution.");
+
+        HttpServletRequest request = attributes.getRequest();
+        Signature signature = joinPoint.getSignature();
+        String functionName = signature.getName();
+
+        logger.info("Request URl:{} {}",request.getRequestURL().toString(),request.getMethod());
+        logger.info("Request Method: {}, {}",signature.getDeclaringType(),functionName);
+        long endTime = System.currentTimeMillis();
+        logger.info("Run time {}",endTime - startTime);
+
         return result;
     }
-    @AfterReturning(value = "publicMethodsFromLoggingPackage()", returning = "result")
-    public void logAfter(JoinPoint joinPoint, Object result) {
+    @After("execution(public  * org.example.springsecuritydemo.controller.HelloController.sayHello())")
+    public void logAfter(JoinPoint joinPoint) {
         logger.info("Logging after method execution.");
-    }
-    @AfterThrowing(pointcut = "publicMethodsFromLoggingPackage()", throwing = "exception")
-    public void logException(JoinPoint joinPoint, Throwable exception) {
-        logger.error("Method threw exception: " + exception.getMessage());
     }
 }
 
