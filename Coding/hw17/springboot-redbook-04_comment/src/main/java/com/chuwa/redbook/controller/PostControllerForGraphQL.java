d921ce70f087363b5cf4611d6c9cb4545f57dd33
Mingxiao <hellomingxiao@gmail.com>
2024-10-21 00:04:26 -0400

    Mingxiao_Li/hw17

--- Coding/hw17/springboot-redbook-04_comment/src/main/java/com/chuwa/redbook/controller/PostControllerForGraphQL.java
+++ Coding/hw17/springboot-redbook-04_comment/src/main/java/com/chuwa/redbook/controller/PostControllerForGraphQL.java
@@ -3,22 +3,13 @@ package com.chuwa.redbook.controller;
 import com.chuwa.redbook.dao.PostRepository;
 import com.chuwa.redbook.entity.Post;
 import com.chuwa.redbook.exception.ResourceNotFoundException;
-import com.chuwa.redbook.payload.CommentDto;
-import com.chuwa.redbook.payload.PostDto;
-import com.chuwa.redbook.service.CommentService;
-import com.chuwa.redbook.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
 
 @Controller
 @RequestMapping("/api/v1/posts")
@@ -28,12 +19,15 @@ public class PostControllerForGraphQL {
 
     @QueryMapping
     public Post getPostById(@Argument Long id) {
-        Post post = postRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Post", "id", id));
-        return post;
+        return postRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Post", "id", id));
     }
 
-//    @MutationMapping
-//    public Post createPost(@Argument String title, @Argument String description, @Argument String content) {
-//        Post post = new Post(title, description, content);
-//    }
+    @MutationMapping
+    public Post createPost(@Argument String title, @Argument String description, @Argument String content) {
+        Post post = new Post();
+        post.setTitle(title);
+        post.setDescription(description);
+        post.setContent(content);
+        return postRepository.save(post);
+    }
 }
